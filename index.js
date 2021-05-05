b6035685c60f423ae280032e6edd95a477acb63e
Keshav Bohra <keshav2016@gmail.com>
2021-05-05 14:11:29 +0530

    added script for cowin availability

---
+++ index.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+const _ = require('lodash');
+const moment = require('moment');
+const dotenv = require('dotenv');
+const sgMail = require('@sendgrid/mail');
+
+dotenv.config()
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const checkForAvailability = async (pincode, date) => {
+    try {
+        const res = await axios({
+            method: 'get',
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${date}`
+        })
+        const centers = _.get(res, 'data.centers', [])
+        filterCentersAvailability(centers)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const filterCentersAvailability = async (centers) => {
+    let isAvailable = 0;
+    let mailText = ''
+    centers.forEach(center => {
+        _.get(center, 'sessions', []).forEach((session) => {
+            if (session.available_capacity > 0 && session.min_age_limit === 18) {
+                isAvailable = 1;
+                mailText = `${mailText}\n${session.vaccine} vaccine is available on ${session.date}`
+                console.log(`${session.vaccine} vaccine is available on ${session.date}`)
+            }
+        })
+    });
+    if (isAvailable) {
+        const msg = {
+            to: process.env.MAIL_TO,
+            from: process.env.MAIL_FROM, // Use the email address or domain you verified above
+            subject: 'Cowin availability',
+            text: mailText
+        };
+        try {
+            await sgMail.send(msg)
+        } catch (error) {
+            console.log('mail did not send')
+        }
+    } else {
+        console.log('not available')
+    }
+}
+
+setInterval(() => {
+    checkForAvailability(process.env.PINCODE, moment().format('DD-MM-YYYY'))
+}, 20000);
