8762b2a887b8102e9acb76f4b450a885599d7be6
Keshav Bohra <keshav2016@gmail.com>
2021-05-12 13:32:51 +0530

    added error log

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const checkForAvailability = async (pincode, date) => {
         const centers = _.get(res, 'data.centers', [])
         filterCentersAvailability(centers)
     } catch (error) {
+        console.log(error)
         console.log('cowin api not called')
     }
 }
